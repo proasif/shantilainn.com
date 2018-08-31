d51ea97f22dc0e0e7c794101a85eba1d25e835f5
proasif <proasifparvez@gmail.com>
2018-08-31 17:07:59 +0530

    Version 1.0.1 - DB Sync

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'shantila');
+define('DB_NAME', 'wp_shantila');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'R38o90z101!');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
