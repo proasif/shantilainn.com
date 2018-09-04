776afe419b3ead3aa3b24fedcd0437de8a440a89
Prodigy <Prodigy@MacBook-Pro.local>
2018-09-04 14:00:03 +0530

    Changed DB Authorization / Salt

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp_shantila');
+define('DB_NAME', 'shantilainn_wp');
 
 /** MySQL database username */
 define('DB_USER', 'webinnshant');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'YwwjLc^J&&%p$pC&5JkPU-Lq%gAAqg5K!RzMfrUG');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
